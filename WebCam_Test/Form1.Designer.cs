90abd8bb1ac0e1b9f30c3b7c69a1579d3d223652
Daniel Mekonnen <daneymail@gmail.com>
2017-11-14 10:40:08 +0100

    Add project files.

---
+++ WebCam_Test/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WebCam_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webCamPicBox = new System.Windows.Forms.PictureBox();
+            this.devicesComboBox = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.saveFileDialog01 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.webCamPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webCamPicBox
+            // 
+            this.webCamPicBox.Location = new System.Drawing.Point(46, 12);
+            this.webCamPicBox.Name = "webCamPicBox";
+            this.webCamPicBox.Size = new System.Drawing.Size(520, 342);
+            this.webCamPicBox.TabIndex = 0;
+            this.webCamPicBox.TabStop = false;
+            // 
+            // devicesComboBox
+            // 
+            this.devicesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.devicesComboBox.Location = new System.Drawing.Point(46, 399);
+            this.devicesComboBox.Name = "devicesComboBox";
+            this.devicesComboBox.Size = new System.Drawing.Size(520, 21);
+            this.devicesComboBox.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(46, 450);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(157, 34);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Camera";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapture.Location = new System.Drawing.Point(223, 450);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(157, 34);
+            this.btnCapture.TabIndex = 3;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(409, 450);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(157, 34);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 496);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.devicesComboBox);
+            this.Controls.Add(this.webCamPicBox);
+            this.Name = "Form1";
+            this.Text = "Web Cam Displayer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webCamPicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox webCamPicBox;
+        private System.Windows.Forms.ComboBox devicesComboBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog01;
+    }
+}
+
---
+++ WebCam_Test/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace WebCam_Test
+{
+    public partial class Form1 : Form
+    {
+
+        private FilterInfoCollection webcam;
+        private VideoCaptureDevice cam;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo videoDevice in webcam)
+            {
+                devicesComboBox.Items.Add(videoDevice.Name);
+            }
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCaptureDevice(webcam[devicesComboBox.SelectedIndex].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+        }
+
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+            webCamPicBox.Image = bit;
+        }
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            saveFileDialog01.InitialDirectory = @"C:\Users\Windows\Pictures";
+            if (saveFileDialog01.ShowDialog() == DialogResult.OK)
+            {
+                webCamPicBox.Image.Save(saveFileDialog01.FileName);
+            }
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cam.IsRunning)
+                {
+                    cam.Stop();
+                    webCamPicBox.Image = null;
+
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Camera is not open");
+            }
+        }
+
+        
+    }
+}
